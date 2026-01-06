478bf7c9e4d353397f0de4153dfa334fc2f94009
jangilimahesh <22wj1a04b4@gniindia.org>
2026-01-06 20:05:09 +0530

    Delete src directory

--- src/auth.js
+++
@@ -1,5 +0,0 @@
-function isAdmin(msg, admins) {
-  return admins.includes(String(msg.from.id));
-}
-
-module.exports = { isAdmin };
--- src/logger.js
+++
@@ -1,9 +0,0 @@
-const axios = require("axios");
-
-module.exports = async function log(token, chatId, text) {
-  await axios.post(`https://api.telegram.org/bot${token}/sendMessage`, {
-    chat_id: chatId,
-    text,
-    disable_web_page_preview: true
-  });
-};
