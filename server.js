ef22ada89c0f230e209023aa727e6057ee13fde5
jangilimahesh <22wj1a04b4@gniindia.org>
2026-01-06 20:06:00 +0530

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-const PORT = process.env.PORT || 3000;
-
-http.createServer((_, res) => {
-  res.end("Bot is running");
-}).listen(PORT, () => {
-  console.log("🌐 Server listening on", PORT);
-});
